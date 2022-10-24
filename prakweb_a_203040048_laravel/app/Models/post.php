f998341edc36f90f64d2ce2ef21403422b22479a
Chairunissa <203040048@mail.unpas.ac.id>
2022-10-24 15:17:38 +0700

    video 5 model, collection & controller

---
+++ prakweb_a_203040048_laravel/app/Models/post.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+
+class Post{
+    private static $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Chairunissa",
+            "body" => "Lorem ipsum, dolor sit amet consectetur 
+            adipisicing elit. Hic dolorum debitis minus
+            asperiores sint, voluptate suscipit consequuntur possimus esse tempore repudiandae? Consequuntur aperiam exercitationem sit molestiae autem, velit id iure numquam, consectetur labore in nemo? Rerum reiciendis, corrupti animi tempora maxime dignissimos fuga neque incidunt, magnam ut perspiciatis soluta, fugit commodi. Quas non laborum, nulla eveniet rem, distinctio blanditiis nihil ducimus repellat dicta unde possimus aperiam numquam aut ullam. Ab quam nostrum, dolore quis earum
+            iusto magnam consequatur laboriosam nemo."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Ompong",
+            "body" => "La suscipit nobis consectetur non voluptas praesentium quod explicabo dignissimos nisi ea quas aspernatur saepe aperiam sed nesciunt molestias repudiandae iste? Earum id, harum nulla voluptatibus saepe dolore repudiandae dicta temporibus inventore accusamus distinctio soluta modi, eius ratione placeat assumenda eos! Officiis, laboriosam, quod, dignissimos temporibus natus aperiam animi odio commodi accusamus minima eius illum. Tempore pariatur voluptates vel impedit a explicabo necessitatibus aliquam, repellendus natus libero eligendi quia fuga iure beatae aut omnis odio illum qui amet quos soluta sit quisquam. Excepturi officia numquam est? 
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, alias est beatae optio adipis i officia numquam est?"
+        ],
+    ];
+
+    public static function all()
+    {
+        return self::$blog_posts;
+    }
+}
--- prakweb_a_203040048_laravel/routes/web.php
+++ prakweb_a_203040048_laravel/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -29,28 +30,9 @@ Route::get('/about', function () {
 });
 
 Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Chairunissa",
-            "body" => "Lorem ipsum, dolor sit amet consectetur 
-        adipisicing elit. Hic dolorum debitis minus
-         asperiores sint, voluptate suscipit consequuntur possimus esse tempore repudiandae? Consequuntur aperiam exercitationem sit molestiae autem, velit id iure numquam, consectetur labore in nemo? Rerum reiciendis, corrupti animi tempora maxime dignissimos fuga neque incidunt, magnam ut perspiciatis soluta, fugit commodi. Quas non laborum, nulla eveniet rem, distinctio blanditiis nihil ducimus repellat dicta unde possimus aperiam numquam aut ullam. Ab quam nostrum, dolore quis earum
-         iusto magnam consequatur laboriosam nemo."
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Ompong",
-            "body" => "La suscipit nobis consectetur non voluptas praesentium quod explicabo dignissimos nisi ea quas aspernatur saepe aperiam sed nesciunt molestias repudiandae iste? Earum id, harum nulla voluptatibus saepe dolore repudiandae dicta temporibus inventore accusamus distinctio soluta modi, eius ratione placeat assumenda eos! Officiis, laboriosam, quod, dignissimos temporibus natus aperiam animi odio commodi accusamus minima eius illum. Tempore pariatur voluptates vel impedit a explicabo necessitatibus aliquam, repellendus natus libero eligendi quia fuga iure beatae aut omnis odio illum qui amet quos soluta sit quisquam. Excepturi officia numquam est? 
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, alias est beatae optio adipis i officia numquam est?"
-        ],
-    ];
-
     return view('posts', [
         "title" => "Posts",
-        "posts" => $blog_posts
+        "posts" => Post::all()
     ]);
 });
 
