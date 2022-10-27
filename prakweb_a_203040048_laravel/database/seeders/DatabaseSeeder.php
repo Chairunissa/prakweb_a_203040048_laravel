5c94fd7f35759a85c32a9420b30acceec46487d3
Chairunissa <chairunissap37@gmail.com>
2022-10-27 22:39:47 +0700

    menyelesaikan video 9 Database Seeder

--- prakweb_a_203040048_laravel/database/seeders/DatabaseSeeder.php
+++ prakweb_a_203040048_laravel/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,67 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'Chairunissa Putri Koswara',
+            'email' => 'chairunissa@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Hafiszah Saidatul',
+            'email' => 'pijah@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa, fugiat labore aliquam esse odio deleniti!',
+            'body' => '<p>Jupiter adalah planet terdekat kelima dari Matahari setelah Merkurius, Venus, Bumi, dan Mars. Planet ini juga merupakan planet terbesar di Tata Surya.Jupiter merupakan raksasa gas dengan massa seperseribu massa Matahari dan dua setengah kali jumlah massa semua planet lain di Tata Surya.</p><p>Planet ini dan raksasa gas lain di Tata Surya (yaitu Saturnus, Uranus, dan Neptunus) kadang-kadang disebut planet Jovian atau planet luar.Jupiter sebagian besar terdiri dari hidrogen dan helium. Seperempat massa Jupiter merupakan helium, walaupun jumlahnya hanya sepersepuluh komposisi Jupiter.</p><p> Planet ini mungkin memiliki inti berbatu yang terdiri dari unsur-unsur berat,[namun tidak memiliki permukaan yang padat layaknya raksasa gas lainnya. Akibat rotasinya yang cepat, planet ini berbentuk bulat pepat (terdapat tonjolan di sekitar khatulistiwa Jupiter)</p>',
+            'category_id' => 1,
+            'user_id' => 1
+
+        ]);
+
+        Post::create([
+            'title' => 'Judul Kedua',
+            'slug' => 'judul-kedua',
+            'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa, fugiat labore aliquam esse odio deleniti!',
+            'body' => '<p>Jupiter adalah planet terdekat kelima dari Matahari setelah Merkurius, Venus, Bumi, dan Mars. Planet ini juga merupakan planet terbesar di Tata Surya.Jupiter merupakan raksasa gas dengan massa seperseribu massa Matahari dan dua setengah kali jumlah massa semua planet lain di Tata Surya.</p><p>Planet ini dan raksasa gas lain di Tata Surya (yaitu Saturnus, Uranus, dan Neptunus) kadang-kadang disebut planet Jovian atau planet luar.Jupiter sebagian besar terdiri dari hidrogen dan helium. Seperempat massa Jupiter merupakan helium, walaupun jumlahnya hanya sepersepuluh komposisi Jupiter.</p><p> Planet ini mungkin memiliki inti berbatu yang terdiri dari unsur-unsur berat,[namun tidak memiliki permukaan yang padat layaknya raksasa gas lainnya. Akibat rotasinya yang cepat, planet ini berbentuk bulat pepat (terdapat tonjolan di sekitar khatulistiwa Jupiter)</p>',
+            'category_id' => 1,
+            'user_id' => 1
+
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ketiga',
+            'slug' => 'judul-ketiga',
+            'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa, fugiat labore aliquam esse odio deleniti!',
+            'body' => '<p>Jupiter adalah planet terdekat kelima dari Matahari setelah Merkurius, Venus, Bumi, dan Mars. Planet ini juga merupakan planet terbesar di Tata Surya.Jupiter merupakan raksasa gas dengan massa seperseribu massa Matahari dan dua setengah kali jumlah massa semua planet lain di Tata Surya.</p><p>Planet ini dan raksasa gas lain di Tata Surya (yaitu Saturnus, Uranus, dan Neptunus) kadang-kadang disebut planet Jovian atau planet luar.Jupiter sebagian besar terdiri dari hidrogen dan helium. Seperempat massa Jupiter merupakan helium, walaupun jumlahnya hanya sepersepuluh komposisi Jupiter.</p><p> Planet ini mungkin memiliki inti berbatu yang terdiri dari unsur-unsur berat,[namun tidak memiliki permukaan yang padat layaknya raksasa gas lainnya. Akibat rotasinya yang cepat, planet ini berbentuk bulat pepat (terdapat tonjolan di sekitar khatulistiwa Jupiter)</p>',
+            'category_id' => 2,
+            'user_id' => 1
+
+        ]);
+
+        Post::create([
+            'title' => 'Judul Keempat',
+            'slug' => 'judul-keempat',
+            'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa, fugiat labore aliquam esse odio deleniti!',
+            'body' => '<p>Jupiter adalah planet terdekat kelima dari Matahari setelah Merkurius, Venus, Bumi, dan Mars. Planet ini juga merupakan planet terbesar di Tata Surya.Jupiter merupakan raksasa gas dengan massa seperseribu massa Matahari dan dua setengah kali jumlah massa semua planet lain di Tata Surya.</p><p>Planet ini dan raksasa gas lain di Tata Surya (yaitu Saturnus, Uranus, dan Neptunus) kadang-kadang disebut planet Jovian atau planet luar.Jupiter sebagian besar terdiri dari hidrogen dan helium. Seperempat massa Jupiter merupakan helium, walaupun jumlahnya hanya sepersepuluh komposisi Jupiter.</p><p> Planet ini mungkin memiliki inti berbatu yang terdiri dari unsur-unsur berat,[namun tidak memiliki permukaan yang padat layaknya raksasa gas lainnya. Akibat rotasinya yang cepat, planet ini berbentuk bulat pepat (terdapat tonjolan di sekitar khatulistiwa Jupiter)</p>',
+            'category_id' => 2,
+            'user_id' => 2
+
+        ]);
     }
 }
--- prakweb_a_203040048_laravel/resources/views/post.blade.php
+++ prakweb_a_203040048_laravel/resources/views/post.blade.php
@@ -1,11 +1,13 @@
 @extends('layouts.main')
 @section('container')
-<article>
-  <h2>{{ $post->title }}</h2>
-  <!-- <h5>{{ $post["author"] }}</h5> -->
-  <p>By. Chairunissa in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name   }}</a></p>
 
-  {!! $post->body !!}
-</article>
-<a href="/blog">Back to posts</a>
+<h2>{{ $post->title }}</h2>
+<!-- <h5>{{ $post["author"] }}</h5> -->
+<p>By. <a href="#" class="text-decoration-none">
+    {{ $post->user->name }}
+  </a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name   }}</a></p>
+
+{!! $post->body !!}
+
+<a href="/blog" class="d-block mt-3">Back to posts</a>
 @endsection
