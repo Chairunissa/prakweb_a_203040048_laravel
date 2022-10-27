bd2c8a6b32a32e5fad5ceb435da5c328fe90dfd0
Chairunissa <chairunissap37@gmail.com>
2022-10-27 21:40:12 +0700

    menyelesaikan video 8 Category & Relationships

--- prakweb_a_203040048_laravel/resources/views/post.blade.php
+++ prakweb_a_203040048_laravel/resources/views/post.blade.php
@@ -3,6 +3,8 @@
 <article>
   <h2>{{ $post->title }}</h2>
   <!-- <h5>{{ $post["author"] }}</h5> -->
+  <p>By. Chairunissa in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name   }}</a></p>
+
   {!! $post->body !!}
 </article>
 <a href="/blog">Back to posts</a>
